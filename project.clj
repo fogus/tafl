f81e4f9a0efed4b45ad725e40acd6886744ce44a
fogus <mefogus@gmail.com>
2017-09-20 14:14:43 -0400

    Moving up support for 19.beta1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject fogus/tafl "0.1.0"
+(defproject fogus/tafl "0.2.0-SNAPSHOT"
   :description ""
   :url "http://www.github.com/fogus/tafl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
   :deploy-repositories [["clojars" {:sign-releases false}]])
