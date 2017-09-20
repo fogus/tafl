83ea2e03ff7c3bbcb41343083414a604ec76f405
fogus <mefogus@gmail.com>
2017-09-20 14:13:46 -0400

    Version 0.1.0 is base relational algebra-style queries.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject fogus/tafl "0.1.0-SNAPSHOT"
+(defproject fogus/tafl "0.1.0"
   :description ""
-  :url "http://example.com/FIXME"
+  :url "http://www.github.com/fogus/tafl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :deploy-repositories [["clojars" {:sign-releases false}]])
