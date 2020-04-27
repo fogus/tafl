0545fbf7ef17edb998098aac9d23eefe7c9b9b44
Fogus <mefogus@gmail.com>
2020-04-27 12:11:31 -0400

    Data model abstraction.

--- src/fogus/tafl.clj
+++ src/fogus/tafl.clj
@@ -8,4 +8,19 @@
   (edn/read-string
    (slurp (io/resource filename))))
 
+(defn- empty-model-db [schema]
+  (let [conn (d/create-conn schema)]
+    (d/db conn)))
 
+(defn build-model [table]
+  (d/db-with
+   (empty-model-db {})
+   table))
+
+(comment
+
+  (def books (read-table "books.edn"))
+
+  (def db (build-model books))
+
+)
