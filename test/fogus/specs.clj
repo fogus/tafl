5a656f249516a9b70afbfc348a47c0bdb91fe1c3
Fogus <mefogus@gmail.com>
2020-04-27 11:15:04 -0400

    Massaging data

--- test/fogus/specs.clj
+++ test/fogus/specs.clj
@@ -1,3 +1,8 @@
 (ns fogus.specs
   (:require [clojure.spec.alpha :as s]))
 
+(s/def :book/author     (s/coll-of string?  :kind set? :into #{}))
+(s/def :book/genre      (s/coll-of keyword? :kind set? :into #{}))
+(s/def :book/title      string?)
+(s/def :personal/rating number?)
+(s/def :personal/tag    (s/coll-of keyword? :kind set? :into #{}))
