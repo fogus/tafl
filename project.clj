cfa8264ab1406d502775b97765e89da49681b2a9
Fogus <mefogus@gmail.com>
2020-04-27 10:15:47 -0400

    Adding deps and dev profile

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://www.github.com/fogus/tafl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [datascript "0.18.11"]]
+  :profiles {:debug {}
+             :dev {:resource-paths ["samples"]}
+             :uberjar {:aot :all}
+             :repl {}}
   :deploy-repositories [["clojars" {:sign-releases false}]])
