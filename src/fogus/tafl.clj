0d430b1f71e8ad0889727570d6bb4192b0bb36a7
Fogus <mefogus@gmail.com>
2020-04-27 12:34:44 -0400

    Getting the bits in order.

--- src/fogus/tafl.clj
+++ src/fogus/tafl.clj
@@ -12,15 +12,33 @@
   (let [conn (d/create-conn schema)]
     (d/db conn)))
 
-(defn build-model [table]
+(defn build-model [schema enums table]
   (d/db-with
-   (empty-model-db {})
-   table))
+   (empty-model-db schema)
+   (concat enums table)))
 
 (comment
 
   (def books (read-table "books.edn"))
+  (def schema {:book/genre {:db/valueType   :db.type/ref
+                            :db/cardinality :db.cardinality/many}})
+  (def enums  #{:genre/scifi :genre/non-tech-tech :genre/humor :genre/found-drama
+                :genre/short-stories :genre/architecture :genre/horror :genre/programming
+                :genre/graphic-novel :genre/sports :genre/biology :genre/folklore
+                :genre/non-fiction :genre/philosophy :genre/fiction})
 
-  (def db (build-model books))
+  (reduce (fn [acc e]
+            (into acc (:book/genre e)))
+          #{}
+          books)
+
+  
+  (def db (build-model schema (map (fn [e] {:db/ident e}) enums) books))
+
+  (d/q '[:find ?title
+         :where
+         [?bid :book/genre :genre/found-drama]
+         [?bid :book/title ?title]]
+       db)
 
 )
