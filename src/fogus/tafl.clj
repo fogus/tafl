13004378f38fb3a99de80df7a89d1aec13943771
Fogus <mefogus@gmail.com>
2020-04-28 09:17:48 -0400

    Extract enums

--- src/fogus/tafl.clj
+++ src/fogus/tafl.clj
@@ -17,21 +17,21 @@
    (empty-model-db schema)
    (concat enums table)))
 
+(defn extract-enums [ks db]
+  (let [key-set (set ks)]
+    (set (flatten (map (apply juxt key-set) db)))))
+
 (comment
 
   (def books (read-table "books.edn"))
-  (def schema {:book/genre {:db/valueType   :db.type/ref
-                            :db/cardinality :db.cardinality/many}})
-  (def enums  #{:genre/scifi :genre/non-tech-tech :genre/humor :genre/found-drama
-                :genre/short-stories :genre/architecture :genre/horror :genre/programming
-                :genre/graphic-novel :genre/sports :genre/biology :genre/folklore
-                :genre/non-fiction :genre/philosophy :genre/fiction})
+  (def schema {:book/genre   {:db/valueType   :db.type/ref
+                              :db/cardinality :db.cardinality/many}
+               :personal/tag {:db/valueType   :db.type/ref
+                              :db/cardinality :db.cardinality/many}})
 
-  (reduce (fn [acc e]
-            (into acc (:book/genre e)))
-          #{}
-          books)
 
+  (= enums
+     (extract-enums [:book/genre :personal/tag] books))
   
   (def db (build-model schema (map (fn [e] {:db/ident e}) enums) books))
 
